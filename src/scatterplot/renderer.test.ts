5b3071e4e8e9d31a46813c13e7d100f3493649c3
alecsferra <alecsferra@protonmail.com>
2022-04-11 19:56:21 +0200

    ScatterPlot renderer

---
+++ src/scatterplot/renderer.test.ts
@@ -0,0 +1,111 @@
+import { groupByShape, SPRenderer } from './renderer';
+
+describe('SPRenderer', () => {
+  it('should respect xAxisSettings', () => {
+    const renderer = new SPRenderer(
+      // @ts-expect-error We aren't mocking the entire  object
+      { domainX: [10, 20] },
+      {},
+    );
+    expect(renderer.xAxis.domain()).toEqual([10, 20]);
+  });
+
+  it('should respect yAxisSettings', () => {
+    const renderer = new SPRenderer(
+      // @ts-expect-error We aren't mocking the entire  object
+      { domainY: [10, 20] },
+      {},
+    );
+    expect(renderer.yAxis.domain()).toEqual([10, 20]);
+  });
+
+  it('should render the axis correctly', () => {
+    // TODO: Figure out how to mock webgl
+  });
+
+  it('should render correctly an empty input', () => {
+    // TODO: Figure out how to mock webgl
+  });
+
+  it('should render correclty on one shape group', () => {
+    // TODO: Figure out how to mock webgl
+  });
+
+  it('should render correclty more than one shape group', () => {
+    // TODO: Figure out how to mock webgl
+  });
+
+  it('should render correclty different colors', () => {
+    // TODO: Figure out how to mock webgl
+  });
+
+  it('should render correclty different sizes', () => {
+    // TODO: Figure out how to mock webgl
+  });
+
+  it('should render correclty a big input', () => {
+    // TODO: Figure out how to mock webgl
+  });
+  it('shouldn\'t render on an invalid reference', () => {
+    // TODO: Figure out how to mock webgl
+  });
+});
+
+describe('method GrupByShape', () => {
+  it('should work on empty collections', () => {
+    expect(groupByShape([])).toEqual(new Map());
+  });
+
+  it('should work on only one group', () => {
+    const input = [
+      { shape: 'a' },
+      { shape: 'a' },
+      { shape: 'a' },
+      { shape: 'a' },
+      { shape: 'a' },
+      { shape: 'a' },
+    ];
+
+    // @ts-expect-error We aren't mocking the entire  object
+    expect(groupByShape(input)).toEqual(new Map([
+      ['a', input],
+    ]));
+  });
+
+  it('should work in multiple groups even when shuffled', () => {
+    const a = [
+      { shape: 'a' },
+      { shape: 'a' },
+      { shape: 'a' },
+      { shape: 'a' },
+      { shape: 'a' },
+      { shape: 'a' },
+    ];
+
+    const b = [
+      { shape: 'b' },
+      { shape: 'b' },
+      { shape: 'b' },
+      { shape: 'b' },
+      { shape: 'b' },
+      { shape: 'b' },
+    ];
+
+    const c = [
+      { shape: 'c' },
+      { shape: 'c' },
+      { shape: 'c' },
+      { shape: 'c' },
+      { shape: 'c' },
+      { shape: 'c' },
+    ];
+
+    // @ts-expect-error We aren't mocking the entire  object
+    expect(groupByShape(a.concat(...b).concat(...c).sort(() => Math.random - 0.5)))
+      .toEqual(new Map([
+        ['a', a],
+        ['b', b],
+        ['c', c],
+      ]));
+  });
+});
---
+++ src/scatterplot/renderer.ts
@@ -0,0 +1,112 @@
+import { scaleLinear, ScaleLinear, select } from 'd3';
+import {
+  chartCartesian, seriesWebglMulti, seriesWebglPoint, webglFillColor,
+} from 'd3fc';
+import { MutableRefObject } from 'react';
+import { GraphableType, GraphableTypeToRepr } from '../model/datatypes';
+import { Renderer } from '../genericview/renderer';
+
+export interface SPREnderablePoint {
+  readonly x: GraphableTypeToRepr<GraphableType.Real>;
+  readonly y: GraphableTypeToRepr<GraphableType.Real>;
+  readonly size: GraphableTypeToRepr<GraphableType.Int>;
+  readonly shape: GraphableTypeToRepr<GraphableType.Shape>;
+  readonly color: GraphableTypeToRepr<GraphableType.Color>;
+}
+
+export interface SPRenderSettings {
+  readonly domainX: [number, number];
+  readonly domainY: [number, number];
+}
+
+export class SPRenderer implements Renderer<SPRenderSettings, SPREnderablePoint> {
+  constructor(
+    private points: SPREnderablePoint[],
+    private settings: SPRenderSettings,
+  ) { }
+
+  updatePoints(p: SPREnderablePoint[]): void {
+    this.points = p;
+  }
+
+  updateSettings(s: SPRenderSettings): void {
+    this.settings = s;
+  }
+
+  render(ref: MutableRefObject<HTMLDivElement>): void {
+    // NOTE: This is needed since right now d3fc does not support the render of
+    // different shapes on the same series
+    // see https://github.com/d3fc/d3fc/issues/1722
+    const grouped = groupByShape(this.points);
+    const chart = this.makeChart(grouped);
+    select(ref.current)
+      .datum(this.points)
+      .call(chart);
+  }
+
+  private makeChart(grouped: Map<SPREnderablePoint['shape'],
+  SPREnderablePoint[]>): WebGLSeries {
+    const series = Array.from(grouped.entries())
+      .map(([shape, points]) => this.makeSeries(shape, points));
+    if (series.length === 0) {
+      series.push(this.makeSeries('star', []));
+    }
+
+    return chartCartesian(this.xAxis, this.yAxis)
+      .webglPlotArea(
+        seriesWebglMulti()
+          .xScale(this.xAxis)
+          .yScale(this.yAxis)
+          .series(series),
+      );
+  }
+
+  private makeSeries(
+    shape: SPREnderablePoint['shape'],
+    points: SPREnderablePoint[],
+  ): WebGLSeries {
+    return seriesWebglPoint()
+      .data(points)
+      .xScale(this.xAxis)
+      .yScale(this.yAxis)
+      .crossValue((p: SPREnderablePoint) => p.x)
+      .mainValue((p: SPREnderablePoint) => p.y)
+      .size((p: SPREnderablePoint) => p.size)
+      .type(shape)
+      .decorate((context: WebGLContext, points: SPREnderablePoint[]) => {
+        const fill = webglFillColor()
+          .value((p: SPREnderablePoint) => p.color)
+          .datum(points);
+        // TODO: Should we support transaprency
+        // const wbgl = context.context()
+        // webgl.enable(webgl.BLEND)
+        fill(context);
+      });
+  }
+
+  public get xAxis(): ScaleLinear<number, number, never> {
+    return scaleLinear().domain(this.settings.domainX);
+  }
+
+  public get yAxis(): ScaleLinear<number, number, never> {
+    return scaleLinear().domain(this.settings.domainY);
+  }
+}
+
+export function groupByShape(elements: SPREnderablePoint[]):
+Map<SPREnderablePoint['shape'], SPREnderablePoint[]> {
+  const accumulator = new Map<SPREnderablePoint['shape'], SPREnderablePoint[]>();
+  for (const element of elements) {
+    const key = element.shape;
+    const group = accumulator.get(key);
+    if (group === undefined) {
+      accumulator.set(key, [element]);
+    } else {
+      group.push(element);
+    }
+  }
+  return accumulator;
+}
+
+type WebGLSeries = any;
+type WebGLContext = any;
