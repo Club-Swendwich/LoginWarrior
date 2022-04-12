4dad7ee8494321fc0f3ded15547c6524a5da8078
alecsferra <alecsferra@protonmail.com>
2022-04-12 15:00:15 +0200

    Test per transformer

---
+++ src/model/transformer.test.ts
@@ -0,0 +1,53 @@
+import { GraphableType, StorableType } from './datatypes';
+import { Transformer } from './transformer';
+
+describe('Transformer', () => {
+  it('should return undefined on an unknown signature', () => {
+    const t = Transformer.new();
+    // @ts-expect-error We aren't mocking the entire object
+    expect(t.get({})).toBeUndefined();
+  });
+
+  it('should retrive correctly an already inserted signature', () => {
+    const t = Transformer.new();
+    const k = { from: StorableType.Int, to: GraphableType.Color, identifier: 'test' };
+    const v = { guid: 420 };
+
+    // @ts-expect-error We aren't mocking the entire object
+    t.add(k, v);
+    expect(t.get(k)).toEqual(v);
+  });
+
+  it('should correctly perform query when is empty', () => {
+    const t = Transformer.new();
+    expect(t.compatibleStorableTypes(GraphableType.Color)).toEqual([]);
+    expect(t.compatibleTransformers(StorableType.Int, GraphableType.Color)).toEqual([]);
+  });
+
+  function sameMembers<T>(a: T[], b: T[]) {
+    expect(a).toEqual(expect.arrayContaining(b));
+    expect(b).toEqual(expect.arrayContaining(a));
+  }
+
+  it('should correctly discriminate between different signatures', () => {
+    const t = Transformer.new();
+    // @ts-expect-error We aren't mocking the entire object
+    t.add({ from: StorableType.Int, to: GraphableType.Color, identifier: 'a' }, {});
+    // @ts-expect-error We aren't mocking the entire object
+    t.add({ from: StorableType.String, to: GraphableType.Color, identifier: 'b' }, {});
+    // @ts-expect-error We aren't mocking the entire object
+    t.add({ from: StorableType.Int, to: GraphableType.Shape, identifier: 'c' }, {});
+    // @ts-expect-error We aren't mocking the entire object
+    t.add({ from: StorableType.Int, to: GraphableType.Color, identifier: 'd' }, {});
+
+    sameMembers(
+      t.compatibleStorableTypes(GraphableType.Color),
+      [StorableType.Int, StorableType.String],
+    );
+
+    sameMembers(
+      t.compatibleTransformers(StorableType.Int, GraphableType.Color),
+      ['a', 'd'],
+    );
+  });
+});
