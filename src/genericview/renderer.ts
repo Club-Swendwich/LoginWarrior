e3eba0bc61ba8b32ee94b2da247a4e04921f1147
alecsferra <alecsferra@protonmail.com>
2022-04-26 16:09:39 +0200

    Clarified tempalte parameters

--- src/genericview/renderer.ts
+++ src/genericview/renderer.ts
@@ -4,10 +4,10 @@ import { MutableRefObject } from 'react';
  * Interface that represents a generic renderer, a components that draw a graph
  * on a specific html reference.
  * @template Settings the graphical preferences of the graph
- * @template RenderablePoints the structure that represents a collection of point that this
+ * @template Renderable the structure that represents something that this
  * renderer is capable of render.
  */
-export interface Renderer<Settings, RenderablePoints> {
+export interface Renderer<Settings, Renderable> {
 
   /**
    * Updates the graphical preferences of the render, does not trigger a
@@ -19,9 +19,9 @@ export interface Renderer<Settings, RenderablePoints> {
   /**
    * Updates the graph point that need to be render, does not trigger a render of
    * the graph
-   * @param p the new points
+   * @param p the new renderable
    */
-  updatePoints(p: RenderablePoints): void;
+  updatePoints(p: Renderable): void;
 
   /**
    * Perform the render of the graph on the specified html reference
