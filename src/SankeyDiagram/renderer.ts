ffd9518860970e822da01d6ad2d35bc585c55c4f
Marco Bustaffa <m.bustaffa@gmail.com>
2022-05-09 15:35:18 +0200

    Render() refactor

--- src/SankeyDiagram/renderer.ts
+++ src/SankeyDiagram/renderer.ts
@@ -37,16 +37,11 @@ export class SKRenderer implements Renderer<RenderSettings, GraphData>{
     public render(ref: MutableRefObject<HTMLDivElement>): void {
         const height = this.settings.height;
         const width = this.settings.width;
-        const _self = this;
-
+        
         const svg = d3.select(ref.current)
                     .append("svg")
                     .attr("width", width)
                     .attr("height", height);
-
-        const formatNumber = d3.format(",.0f"),
-        format = function (d: any) { return formatNumber(d) + " TWh"; },
-        color = d3.scaleOrdinal(d3.schemeCategory10);
 
         // Creo il grafico
         const graph = d3Sankey.sankey()
@@ -54,43 +49,60 @@ export class SKRenderer implements Renderer<RenderSettings, GraphData>{
                         .nodePadding(10)
                         .extent([[1, 1], [width - 1, height - 6]]);
         graph(this.SKRenderableData);
+        const links = this.createLinks(svg);
+        this.createNodes(svg, graph, links);
 
+    }
 
-        // Creo i collegamenti tra i nodi
-        const link = svg.append("g")
-                        .selectAll(".link")
-                        .data(this.SKRenderableData.links)
-                        .enter().append("path")
-                            .attr("class", "link")
-                            .attr("d", d3Sankey.sankeyLinkHorizontal())
-                            .attr("stroke", "#000")
-                            .attr("stroke-opacity", 0.2)
-                            .attr("fill", "none")
-                            .attr("stroke-width", function(d: any) { 
-                                return Math.max(1, d.width); 
-                            });
+    private createLinks(svg: d3.Selection<SVGSVGElement, unknown, null, undefined>) {     
+        const link = svg.append("g").selectAll(".link")
+                    .data(this.SKRenderableData.links)
+                    .enter().append("path")
+                    .attr("class", "link")
+                    .attr("d", d3Sankey.sankeyLinkHorizontal())
+                    .attr("stroke", "#000")
+                    .attr("stroke-opacity", 0.2)
+                    .attr("fill", "none")
+                    .attr("stroke-width", function(d: any) { 
+                        return Math.max(1, d.width); 
+                    });
+        /**
+         * TODO: Mouseover sui path per scurire il collegamento
+         */
+        return link;
+    }
+
+    private createNodes(svg: d3.Selection<SVGSVGElement, unknown, null, undefined>, 
+                        graph: d3Sankey.SankeyLayout<d3Sankey.SankeyGraph<{}, {}>, {}, {}>, 
+                        link: d3.Selection<SVGPathElement, SLink, SVGGElement, unknown>): void {
+        
+        const _self = this;
+        const color = d3.scaleOrdinal(d3.schemeCategory10);
         // Creo i nodi 
         const node = svg.append("g")
-                        .selectAll(".node")
-                        .data(this.SKRenderableData.nodes)
-                        .enter().append("rect")
-                        .attr("class", "node")
-                        .attr("x", function (d: any) { return d.x0; })
-                        .attr("y", function (d: any) { return d.y0; })
-                        .attr("height", function (d: any) { return d.y1 - d.y0; })
-                        .attr("width", function (d: any) { return d.x1 - d.x0; })
-                        .attr("fill", function (d: any) { return color(d.name.replace(/ .*/, "")); })
-                        .attr("stroke", "#000");
-        
-        // Make them draggable
+                .selectAll(".node")
+                .data(this.SKRenderableData.nodes)
+                .enter().append("rect")
+                .attr("class", "node")
+                .attr("x", function (d: any) { return d.x0; })
+                .attr("y", function (d: any) { return d.y0; })
+                .attr("height", function (d: any) { return d.y1 - d.y0; })
+                .attr("width", function (d: any) { return d.x1 - d.x0; })
+                .attr("fill", function (d: any) { return color(d.name.replace(/ .*/, "")); })
+                .attr("stroke", "#000");
+
+        // Make nodes draggable
+        /**
+         * TODO: stop drag when it cross the borders 
+         */
         svg.selectAll<SVGElement, unknown>(".node")
         .call(
             d3.drag<SVGElement, unknown>()
             .subject(d => d)
             .on('start', function () { this.parentNode?.appendChild(this); })
             .on('drag', function (event, d: any) {
-                var rectY: number = +d3.select(this).attr("y");
-                var rectX: number = +d3.select(this).attr("x");
+                var rectY = +d3.select(this).attr("y");
+                var rectX = +d3.select(this).attr("x");
                 d.y0 = d.y0 + event.dy;
                 d.x1 = d.x1 + event.dx;
                 d.x0 = d.x0 + event.dx;
@@ -100,6 +112,14 @@ export class SKRenderer implements Renderer<RenderSettings, GraphData>{
                 graph.update(_self.SKRenderableData);
                 link.attr('d', d3Sankey.sankeyLinkHorizontal());
             })
-        )
+        );
+    }
+    
+    public get svgWidth(): number {
+        return this.settings.width;
+    }
+
+    public get svgHeight(): number {
+        return this.settings.height;
     }
 }
