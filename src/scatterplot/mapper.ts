f059547f8ffe236f53fb3aa81c5c1112a35a7b42
samuelepozzebon <72387285+samuelepozzebon@users.noreply.github.com>
2022-05-21 12:57:23 +0200

    Update mapper.ts
    fix per pull requestt

--- src/scatterplot/mapper.ts
+++ src/scatterplot/mapper.ts
@@ -13,7 +13,7 @@ interface MapFnAccumulator {
 }
 
 // TODO: implement map tests
-export default class SPMapper implements Mapper<SPDimensions, SPREnderablePoint[]> {
+export class SPMapper implements Mapper<SPDimensions, SPREnderablePoint[]> {
   private mapFn : MapFnAccumulator;
 
   constructor(
